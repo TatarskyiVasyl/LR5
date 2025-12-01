8e7d92f52b4778d3dba2c23b08fe90f2ce4866ab
TatarskyiVasyl <tatarskyi.vasyl.kn@dspu.edu.ua>
2025-12-01 11:19:44 +0200

    Add files via upload

---
+++ LR 5.3.cpp
@@ -0,0 +1,69 @@
+﻿// Lab_05_3.cpp
+// Татарський Василь Петрович
+// Лабораторна робота №5.3
+// Функції, що містять розгалуження та цикли з рекурентними співвідношеннями
+// Варіант 28
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double qp, qk, q;
+    int n;
+
+    cout << "qp = "; cin >> qp;
+    cout << "qk = "; cin >> qk;
+    cout << "n = "; cin >> n;
+
+    double dq = (qk - qp) / n;
+
+    // Заголовок таблиці
+    cout << "\n===========================================" << endl;
+    cout << "          Таблиця значень виразу" << endl;
+    cout << "===========================================" << endl;
+
+    // Шапка таблиці
+    cout << setw(12) << "q" << setw(19) << "z" << endl;
+    cout << string(43, '-') << endl;
+
+    q = qp;
+    while (q <= qk)
+    {
+        double z = h(q + 1) + h(q * q + 1) + h(q * q) * h(q * q);
+        cout << setw(15) << fixed << setprecision(6) << q
+            << setw(20) << setprecision(8) << z << endl;
+        q += dq;
+    }
+
+    return 0;
+} 
+
+
+double h(double x)
+{
+    if (abs(x) >= 1)
+    {
+        double c = cos(x);
+        return (c + 1) / (c * c + 1);
+    }
+    else
+    {
+        double S = 1;
+        double a = 1;
+
+        for (int j = 1; j <= 6; j++)
+        {
+            a *= -x * x / ((2 * j - 1) * (2 * j));
+            S += a;
+        }
+
+        return S / cos(x);
+    }
+}
---
+++ LR5.5.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_05_5.cpp
+// Татарський Василь Петрович
+// Лабораторна робота №5.5
+// Рекурсивні функції
+// Варіант 28
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int A(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    }
+    if (n == 0) {
+        return A(m - 1, 1);
+    }
+    return A(m - 1, A(m, n - 1));
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    cout << "Функція Аккермана" << endl << endl;
+    cout << "A(0, 0) = " << A(0, 0) << endl;
+    cout << "A(1, 5) = " << A(1, 5) << endl;
+    cout << "A(2, 5) = " << A(2, 5) << endl;
+    cout << "A(3, 3) = " << A(3, 3) << endl;
+    cout << "A(4, 0) = " << A(4, 0) << endl;
+    return 0;
+}
